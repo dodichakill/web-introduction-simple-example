867edd4517db3e59588f5cc8bee2aad459da0923
dodi herlangga <dodiherlangga91@gmail.com>
2020-08-20 18:13:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const btngame = document.querySelector('.btngame');
+const dgame = document.querySelector('#dgame');
+btngame.addEventListener('click', function(){
+    dgame.innerHTML = `<iframe width="100%" src="https://www.youtube.com/embed/d-bUCjI3anw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+});
+
+const btncoding = document.querySelector('.btncoding');
+const dcoding = document.querySelector('#dcoding');
+btncoding.addEventListener('click', function(){
+    dcoding.innerHTML = `<iframe width="100%" src="https://www.youtube.com/embed/Ix2b_qFYfAA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+});
+
+const btnsilat = document.querySelector('.btnsilat');
+const dsilat = document.querySelector('#dsilat');
+btnsilat.addEventListener('click', function(){
+    dsilat.innerHTML = `<iframe width="100%" src="https://www.youtube.com/embed/4tvtTsx4LUU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+});
+
+const btntani = document.querySelector('.btntani');
+const dtani = document.querySelector('#dtani');
+btntani.addEventListener('click', function(){
+    dtani.innerHTML = `<iframe width="100%" src="https://www.youtube.com/embed/zv5QCZ55gDs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+});
